9081326503bce2e6e0a20321b1cff777d64ce152
Jocy Wolff <jocywolff@gmail.com>
2021-03-15 18:09:59 +0100

    chore: added comment

--- src/rosters.ts
+++ src/rosters.ts
@@ -1,6 +1,12 @@
 import Roster from "./models/roster/Roster";
 import {drivers, teams} from "./generate";
 
+/*
+ * ===========================================
+ *          THIS FILE IS GIT-IGNORED
+ * ===========================================
+ */
+
 const roster = new Roster();
 roster.drivers = [drivers.norris,drivers.hamilton,drivers.ocon,drivers.gasly,drivers.perez];
 roster.team = teams.alpha;
