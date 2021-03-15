4ad19a9d92f46c6b7b0e30740adaf205e0185181
Jocy Wolff <jocywolff@gmail.com>
2021-03-15 14:24:46 +0100

    fix: more typos

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import {drivers, teams} from "./generate";
 import Roster from "./models/roster/Roster";
 import Weekend from "./models/weekend/Weekend";
 
-const roaster = new Roster();
-roaster.drivers = [drivers.verstappen,drivers.schumacher,drivers.latifi,drivers.norris,drivers.gasly];
-roaster.team = teams.mercedes;
+const roster = new Roster();
+roster.drivers = [drivers.verstappen,drivers.schumacher,drivers.latifi,drivers.norris,drivers.gasly];
+roster.team = teams.mercedes;
 
 const weekend = new Weekend();
 weekend.drivers = Object.values(drivers);
 weekend.simulate();
-console.log(weekend.getScore(roaster));
+console.log(weekend.getScore(roster));
